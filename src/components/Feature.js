c81aa795e7829f9bfdaa8afac510e73afba9ea5b
Troy Schneringer <troy.schneringer@ctl.io>
2016-09-21 00:35:04 -0500

    fix feature colmns with smaller images

--- src/components/Feature.js
+++ src/components/Feature.js
@@ -8,12 +8,12 @@ export default React.createClass({
             <div className={"feature " + this.props.color}>
                 <div className="container">
                     <div className="row">
-                        <div className={"col-md-7" + (this.props.align == 'left' ? " col-md-push-5" : "") }>
+                        <div className={"col-md-6" + (this.props.align == 'left' ? " col-md-push-4" : " col-md-push-1") }>
                             <h2>{this.props.title}</h2>
                             <p>{this.props.children}</p>
                             
                         </div>
-                        <div className={"col-md-5" + (this.props.align == 'left' ? " col-md-pull-7" : "") }>
+                        <div className={"col-md-4" + (this.props.align == 'left' ? " col-md-pull-6" : " col-md-push-1") }>
                             <img className="img-circle img-responsive center-block" alt="500x500" src={this.props.imageUrl} />
                         </div>
                     </div>
