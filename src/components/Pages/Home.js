d053c38f6ac883a9f4b79f3948bca6f422dcab6c
Troy Schneringer <troy.schneringer@ctl.io>
2016-09-25 21:01:30 -0700

    update feature images on home page

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -12,10 +12,10 @@ export default React.createClass({
           <h1>Art By Mom</h1>
           <h2>Real Art | <em>Real Fun</em></h2>
         </Jumbotron>
-        <Feature title="Everyone is an Artist" imageUrl="http://placehold.it/250x250" color="yellow">
+        <Feature title="Everyone is an Artist" imageUrl="images/feature-pencils.jpg" color="yellow">
           Thereafter he walked very carefully, with his eyes on the road, and when he saw a tiny ant toiling by he would step over it, so as not to harm it.  The Tin Woodman knew very well he had no heart, and therefore he took great care never to be cruel or unkind to anything. "You people with hearts," he said, "have something to guide you, and need never do wrong; but I have no heart, and so I must be very careful.  When Oz gives me a heart of course I needn't mind so much.\" They were obliged to camp out that night under a large tree in the forest, for there were no houses near.
         </Feature>
-        <Feature title="Everyday is Art Class" imageUrl="http://placehold.it/250x250" align="left" color="blue">
+        <Feature title="Everyday is Art Class" imageUrl="images/feature-tools.jpg" align="left" color="blue">
           Thereafter he walked very carefully, with his eyes on the road, and when he saw a tiny ant toiling by he would step over it, so as not to harm it.  The Tin Woodman knew very well he had no heart, and therefore he took great care never to be cruel or unkind to anything. "You people with hearts," he said, "have something to guide you, and need never do wrong; but I have no heart, and so I must be very careful.  When Oz gives me a heart of course I needn't mind so much.\" They were obliged to camp out that night under a large tree in the forest, for there were no houses near.
         </Feature>
         <hr />
