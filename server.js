0e62a977b7ca8323be6864f618a0e7451bb7dd21
Troy Schneringer <troy.schneringer@ctl.io>
2016-11-22 11:22:39 -0800

    add auth to api

--- server.js
+++ server.js
@@ -1,15 +1,32 @@
 require('babel-register');
 
-var express = require('express');
+// load environment from file
+var dotenv = require('dotenv');
+dotenv.load();
+
+// express
+const express = require('express');
 const app = express();
 
+// auth
+const jwt = require('express-jwt');
+app.use('/api', jwt(
+    { 
+        secret: new Buffer(process.env.AUTH0_CLIENT_SECRET, 'base64'),
+        audience: process.env.AUTH0_CLIENT_ID,
+        issuer: 'https://artbymom.auth0.com/' 
+    }));
+
+// json body
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-var projects = require('./routes/projects.js');
-
+// ui
 app.use('/', express.static('www'));
+
+// api
+var projects = require('./routes/projects.js');
 app.use('/api/projects', projects);
 
 app.listen(process.env.PORT || 3000);
